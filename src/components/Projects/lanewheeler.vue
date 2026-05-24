9fd299936cecb2d8031b838e13bf9a9acaf9c322
Lane Wheeler <lanecwheeler@gmail.com>
2026-05-24 14:44:53 -0700

    updating "lanewheeler" project to include the stack

--- src/components/Projects/lanewheeler.vue
+++ src/components/Projects/lanewheeler.vue
@@ -1,6 +1,10 @@
 <template>
     <p>
         The very site you're lookin' at! I love css and unique designs, and had a whole lotta fun making this!
+        <br/>
+        On top of that, this website actually exists in a stack of several other services, such as cloud based file share,
+        a password manager, an api service, a container management service, etc...
+        <br/>
         If you wanna see something fun, try punching in the <span title="↑ ↑ ↓ ↓ ← → ← → B A">Konami Code</span>
         with your keyboard ;)
     </p>
